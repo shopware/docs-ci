4960dd95a80224e33c0ef79d9a84a6dd6723cc36
Christian Rades <c.rades@shopware.com>
2022-12-12 10:08:30 +0100

    moved script to action

--- set_descriptions.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/env bash
-
-for k in $(yq '. | keys | .[]' descriptions.yml); do
-    newDescription=$(yq ".\"$k\"" descriptions.yml);
-    yq  -I 4 -o json -iP "$k.description = \"${newDescription}\"" storeapi.json;
-done
