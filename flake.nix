bba9cdfe60a669fb400e442c022c4c4d3647dbac
Christian Rades <c.rades@shopware.com>
2022-12-12 09:54:39 +0100

    added nix and description script

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Tooling for the api docs";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    devShell.x86_64-linux = pkgs.mkShell {
+      buildInputs = with pkgs; [yq-go shellcheck];
+    };
+  };
+}
